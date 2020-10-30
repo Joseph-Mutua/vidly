2a9d75279125ef29bac0783ed0bc4ac5e6b4e68e
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-30 17:31:26 +0300

    Displaying validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -16,13 +16,12 @@ class LoginForm extends Component {
   };
 
   validate = () => {
+    const { account } = this.state;
     const errors = {};
 
-    const { account } = this.state;
+    if (!account.username) errors.username = "Username is required!";
 
-    if (account.username.trim === "") errors.username = "Username is required!";
-
-    if (account.password.trim === "")
+    if (!account.password)
       errors.password = "Password  is required!";
 
     return Object.keys(errors).length === 0 ? null : errors;
@@ -31,13 +30,13 @@ class LoginForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -47,12 +46,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
