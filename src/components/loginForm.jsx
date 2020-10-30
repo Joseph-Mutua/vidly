c8b95802fcc1ba5073effa6f98b8688857c9e84f
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-30 15:56:28 +0300

    A basic validation implementation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -5,19 +5,35 @@ class LoginForm extends Component {
 
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
 
-  handleChange = ({currentTarget: input}) => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account });
   };
 
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+
+    if (account.username.trim === "") 
+        errors.username = "Username is required!";
+
+    if (account.password.trim === "")
+      errors.password = "Password  is required!";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
-    const username = this.username.value;
-
-    console.log("Submitted");
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors });
+    if (errors) return;
   };
 
   render() {
