bcc9c4a5897dddfbbf591b129f5ed19e182b31c5
Joseph Mutua <Mutuaj793@gmail.com>
2020-10-30 17:07:43 +0300

    Extracting a reusable input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   username = React.createRef();
@@ -19,8 +20,7 @@ class LoginForm extends Component {
 
     const { account } = this.state;
 
-    if (account.username.trim === "") 
-        errors.username = "Username is required!";
+    if (account.username.trim === "") errors.username = "Username is required!";
 
     if (account.password.trim === "")
       errors.password = "Password  is required!";
@@ -42,29 +42,18 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              autoFocus
-              value={account.username}
-              onChange={this.handleChange}
-              name="username"
-              id="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              value={account.password}
-              onChange={this.handleChange}
-              id="password"
-              name="password"
-              type="text"
-              className="form-control"
-            />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
